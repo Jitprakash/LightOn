666ebc3d4089b9de9f094ef5806f73be44fc4a4e
Jitprakash <jitprakash2020@gmail.com>
2025-09-09 00:06:49 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import {styles} from "@/styles/auth.style";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textDec}>Hello World dear</Text>
+    </View>
+  );
+}
+
---
+++ styles/auth.style.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textDec: { fontSize: 20, fontWeight: "bold" },
+});
