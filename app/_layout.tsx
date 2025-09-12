ac19a5c207e8a8e20540242a7205d5843dbe1c78
Jitprakash <jitprakash2020@gmail.com>
2025-09-12 21:27:34 +0530

    Covex DB connected

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,22 @@
 import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
-import {ClerkProvider} from '@clerk/clerk-expo'
+import {ClerkProvider, useAuth} from '@clerk/clerk-expo'
 import {tokenCache} from '@clerk/clerk-expo/token-cache'
 import InitialLayout from "@/components/InitialLayout";
+import {ConvexReactClient} from "convex/react";
+import {ConvexProviderWithClerk} from "convex/react-clerk";
 
 export default function RootLayout() {
+
+    const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL as string);
     return (
         <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-            <SafeAreaProvider>
-                <SafeAreaView style={{flex: 1, backgroundColor: "black"}}>
-                    <InitialLayout/>
-                </SafeAreaView>
-            </SafeAreaProvider>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                <SafeAreaProvider>
+                    <SafeAreaView style={{flex: 1, backgroundColor: "black"}}>
+                        <InitialLayout/>
+                    </SafeAreaView>
+                </SafeAreaProvider>
+            </ConvexProviderWithClerk>
         </ClerkProvider>
     );
 }
